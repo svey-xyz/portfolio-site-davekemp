9eafc72d01eb07ba79fab831c434643e5f659449
kempdave <dkemp@torontomu.ca>
2024-08-19 17:53:37 -0400

    Uploading old Image Matter website content

---
+++ public/imagematter/xhtml/imagematter.js
@@ -0,0 +1,3 @@
+<!--
+document.write ('<ul><li><a href="../xhtml/news.htm">news</a></li><li><a href="../xhtml/gallery.htm">gallery</a></li><li><a href="../xhtml/statement.htm">statement</a></li><li><a href="../xhtml/essay.htm">essay</a></li><li><a href="../xhtml/acknowledge.htm">acknowledgments</a></li><li><a href="../xhtml/usage.htm">image-usage</a></li><li><a href="../xhtml/links.htm">links</a></li><li><a href="../xhtml/contact.htm">contact</a></li></ul>')
+-->
